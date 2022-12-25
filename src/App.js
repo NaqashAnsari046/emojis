562196b20ac2436d4a3a358462f0b914af2e7ae7
NaqashAnsari046 <106650957+NaqashAnsari046@users.noreply.github.com>
2022-12-26 01:38:04 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import EmojiPicker, {
+  EmojiStyle,
+  SkinTones,
+  Theme,
+  Categories,
+  EmojiClickData,
+  Emoji,
+  SuggestionMode,
+  SkinTonePickerLocation
+} from "emoji-picker-react";
+import { useState } from "react";
 
-function App() {
+ const App = () => {
+  const [selectedEmoji, setSelectedEmoji] = useState  ("");
+
+  function onClick(emojiData: EmojiClickData, event: MouseEvent) {
+    setSelectedEmoji(emojiData.unified);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Emoji Picker React 4 Demo</h2>
+      <div className="show-emoji">
+        Your selected Emoji is:
+        {selectedEmoji ? (
+          <Emoji
+            unified={selectedEmoji}
+            emojiStyle={EmojiStyle.APPLE}
+            size={22}
+          />
+        ) : null}
+        <span>hello {selectedEmoji}</span>
+      </div>
+
+      <EmojiPicker
+        onEmojiClick={onClick}
+        autoFocusSearch={false}
+        // theme={Theme.AUTO}
+        // searchDisabled
+        // skinTonePickerLocation={SkinTonePickerLocation.PREVIEW}
+        // height={350}
+        // width="50%"
+        // emojiVersion="0.6"
+        // lazyLoadEmojis={true}
+        // previewConfig={{
+        //   defaultCaption: "Pick one!",
+        //   defaultEmoji: "1f92a" // 🤪
+        // }}
+        // suggestedEmojisMode={SuggestionMode.RECENT}
+        // skinTonesDisabled
+        // searchPlaceHolder="Filter"
+        // defaultSkinTone={SkinTones.MEDIUM}
+        // emojiStyle={EmojiStyle.NATIVE}
+        // categories={[
+        //   {
+        //     name: "Fun and Games",
+        //     category: Categories.ACTIVITIES
+        //   },
+        //   {
+        //     name: "Smiles & Emotions",
+        //     category: Categories.SMILEYS_PEOPLE
+        //   },
+        //   {
+        //     name: "Flags",
+        //     category: Categories.FLAGS
+        //   },
+        //   {
+        //     name: "Yum Yum",
+        //     category: Categories.FOOD_DRINK
+        //   }
+        // ]}
+      />
     </div>
   );
 }
 
-export default App;
+export default App
